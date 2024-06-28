27cc036e25c87c16556f1cf49b99529c35f14412
Bobdedob <K102@clayesmore.com>
2024-06-28 16:58:46 +0100

    Update random password.py.py

--- random password.py.py
+++ random password.py.py
@@ -1,7 +1,1 @@
-import random
-password = [ '1','0','5','6', ]
-name = [ '1','1','1','1', ]
-accesscode = input('...|')
-if accesscode == random.choice(password) :
-	print('hello, ',random.choice(name),'importing code')
-	print('proccessing...')
+
